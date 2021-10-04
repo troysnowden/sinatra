615f4726e963a97ba7ac9aad185dde26b506624f
Tom Baxter <thomas99baxter@gmail.com>
2021-10-04 15:26:16 +0100

    add /cat route

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "sinatra", "~> 2.1"
+
+gem "sinatra-contrib", "~> 2.1"
--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
-get '/about' do
+
+get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "penguin feet"
+end
+
+get '/cat' do
+  "<img style='border-style: dashed; border-color: red;' src='https://i.imgur.com/jFaSxym.png'>
+  </img>"
 end
