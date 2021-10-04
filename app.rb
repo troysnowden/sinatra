9df98548479501c20a4d2b068da3bc36d91c2ea2
Tom Baxter <thomas99baxter@gmail.com>
2021-10-04 15:53:29 +0100

    add named-cat

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-get '/' do
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
-get '/cat' do
-  erb(:cat)
+get '/random-cat' do
+  @name =  ["Amigo", "Misty", "Almond"][rand(0..2)]
+  erb(:index)
 end
